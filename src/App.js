4b8c73b1c53e4383e59886741f0e99b8ddaf1bd8
shivhub09 <shivnagori2020@gmail.com>
2023-07-26 19:45:27 +0530

    lec 4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+let name = "Shivam";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+       <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <div className='text'>
+        <h1>Hello {name}</h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, eius! Ut dolores unde id alias dignissimos laudantium, debitis repellat iure neque ducimus?
+      </div>
     </div>
   );
 }
